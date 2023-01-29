ca4feafa3484ffd88eb98c296649149ee750f1df
djkim <shurian@naver.com>
2023-01-30 00:58:50 +0900

    eslingignore에 next.config.js추가 운영환경일때 console.log 제거 하게 추가

--- next.config.js
+++ next.config.js
@@ -5,9 +5,25 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: true,
   poweredByHeader:false,
+  compiler:{
+    removeConsole:{ // console.log 제거 옵션
+      exclude: ['error'],
+    }
+  }
   // "distDir": "build",
   // "exportTrailingSlash": true,
   // "assetPrefix": "https://~~~"
 }
 
-module.exports = nextConfig;
+const initNextConfig = () =>{
+
+  if(process.env.NODE_ENV !== 'production'){
+    nextConfig.compiler.removeConsole = false;
+  }
+
+  // console.log(`nextConfig = `, nextConfig);
+  
+  return nextConfig;
+}
+
+module.exports = initNextConfig();
--- src/pages/api/webToken.ts
+++ src/pages/api/webToken.ts
@@ -6,25 +6,25 @@ import { setCookie } from '@/utils/cookies';
 logger.setInstanceName("webToken API");
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<JwtTokenType>,
+    req: NextApiRequest,
+    res: NextApiResponse<JwtTokenType>,
 ): Promise<void> {
 
     let remoteAddress = "";
 
-    if( typeof req?.headers !== "undefined" ){
-        if(typeof req.headers['x-real-ip'] !== "undefined"){
+    if (typeof req?.headers !== "undefined") {
+        if (typeof req.headers['x-real-ip'] !== "undefined") {
             remoteAddress = req.headers['x-real-ip'] as string;
-        }else if(typeof req.headers['x-forwarded-for'] !== "undefined"){
+        } else if (typeof req.headers['x-forwarded-for'] !== "undefined") {
             remoteAddress = req.headers['x-forwarded-for'] as string;
-        }else if(req?.socket?.remoteAddress !== null){
-            remoteAddress = req?.socket?.remoteAddress??"";
-        }else{
+        } else if (req?.socket?.remoteAddress !== null) {
+            remoteAddress = req?.socket?.remoteAddress ?? "";
+        } else {
             remoteAddress = req?.headers.host ?? "";
         }
     }
 
-    const jwtToken = await jwtUtil.sign({userIdx:1, email:"kdj@mail.com", remoteAddress:remoteAddress});
+    const jwtToken = await jwtUtil.sign({ userIdx: 1, email: "kdj@mail.com", remoteAddress });
 
     res.setHeader('Authorization', `Bearer ${jwtToken.accessToken}`);
 
